47c66e7426d95da3ce5f430edd825e6242e59b5f
Thomas Roell <grumpyoldpizza@gmail.com>
2016-11-25 20:21:44 -0700

    Remove cache prefetching (FLASH_ACR_PRFTEN)

--- system/STM32L4xx/Source/stm32l4_system.c
+++ system/STM32L4xx/Source/stm32l4_system.c
@@ -1195,7 +1195,7 @@ bool stm32l4_system_configure(uint32_t lseclk, uint32_t hseclk, uint32_t hclk, u
     }
     else
     {
-	FLASH->ACR = FLASH_ACR_PRFTEN | FLASH_ACR_ICEN | FLASH_ACR_DCEN | latency;
+	FLASH->ACR = FLASH_ACR_ICEN | FLASH_ACR_DCEN | latency;
     }
 
     if (!(apb1enr1 & RCC_APB1ENR1_PWREN))
